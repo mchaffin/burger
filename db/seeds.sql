855863039177da8bfeacc1ce3a8c922e59d58c71
Mark Chaffin <mark.chaffin@akimbosolutions.com>
2018-06-20 18:04:53 -0500

    First working version

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Double Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Le Royal with Cheese', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mustard Pickle Onion', true);
