a29021d05217bd7b94215c5b06674f238d3fc2a7
Mark Chaffin <mark.chaffin@akimbosolutions.com>
2018-06-23 08:28:58 -0500

    Adding Heroku DB connection - let's see if it works

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,28 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 8889,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 8889,
+//   user: "root",
+//   password: "root",
+//   database: "burgers_db"
+// });
+
+var connection;
+if(process.env.JAWSDB_URL) {
+  //Heroku deployment
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  //local host
+    connection = mysql.createConnection({
+        root: 3000,
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "db_name",
+    });
+};
 
 // Make connection.
 connection.connect(function(err) {
